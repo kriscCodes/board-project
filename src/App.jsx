30508cde0da8a455693c61e9bf7ef980402d28d2
kriscCodes <110057928+kriscCodes@users.noreply.github.com>
2023-09-22 23:40:43 -0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+import Board from './Board.jsx';
+export default function App() {
+	return (
+		<>
+			<Board ></Board>
+		</>
+	);
+}
---
+++ src/FoodCard.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+export default function FoodCard(props) {
+	return (
+		<>
+			<div className = "foodCards">
+				<img className = "cardImage" src={props.source}></img>
+				<h3 className = "cardName">{props.name}</h3>
+				<h4 className = "cardCuisine">{props.cuisine}</h4>
+					<a href={props.menu}>
+				<button className = "cardButton">View Menu :D</button>
+				</a>
+			</div>
+		</>
+	);
+}
